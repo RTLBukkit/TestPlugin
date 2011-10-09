79f8bf40eddfdbf305bdd42263549a87363ec809
Olloth <olloth@gmail.com>
2011-10-08 21:46:28 -0400

    Fix?

--- src/main/java/org/getspout/testplugin/TestBlock.java
+++ src/main/java/org/getspout/testplugin/TestBlock.java
@@ -1,20 +1,17 @@
 package org.getspout.testplugin;
 
-import org.bukkit.Material;
 import org.bukkit.plugin.Plugin;
-import org.getspout.spoutapi.SpoutManager;
-import org.getspout.spoutapi.inventory.GenericCubeBlockDesign;
-import org.getspout.spoutapi.inventory.ItemManager;
+import org.getspout.spoutapi.block.design.GenericCubeBlockDesign;
+import org.getspout.spoutapi.block.design.Texture;
 import org.getspout.spoutapi.material.block.GenericCubeCustomBlock;
 
 public class TestBlock extends GenericCubeCustomBlock {
-	public static int[] faces = new int[]{0,0,0,0,0,0};
+	
+	public static String textureUrl = "http://dl.dropbox.com/u/40267690/quartz.png";
+	public static Texture texture = new Texture(textureUrl, 16, 16, 16);
 
 	public TestBlock(Plugin plugin) {
-		super(plugin, "CustomBlock", new GenericCubeBlockDesign(plugin.getDescription().getName(), "http://dl.dropbox.com/u/40267690/quartz.png", 16, faces), false, 0);
-		ItemManager im = SpoutManager.getItemManager();
-		im.setCustomItemBlock(getCustomId(), getRawId(), (short) 0);
-		im.setItemTexture(Material.FLINT, (short) getCustomId(), plugin, "http://dl.dropbox.com/u/40267690/quartz.png");
+		super(plugin, "CustomBlock", true, new GenericCubeBlockDesign(plugin, texture, 0), 0);
 	}
 
 }
