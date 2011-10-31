abb74af8f2b1bb20826f6bc225dfc2665adca3e7
Afforess <afforess@gmail.com>
2011-10-31 14:11:27 -0300

    Testing dirty procedure.

--- src/main/java/org/getspout/testplugin/TestScreenListener.java
+++ src/main/java/org/getspout/testplugin/TestScreenListener.java
@@ -8,13 +8,20 @@ import org.getspout.spoutapi.gui.GenericLabel;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class TestScreenListener extends ScreenListener {
+	private int tries = 0;
 	
 	@Override
 	public void onButtonClick(ButtonClickEvent event) {
 		if (event.getButton() instanceof GenericButton && event.getButton().getText().equals("Test")) {
-			((SpoutPlayer) event.getPlayer()).getMainScreen().closePopup();
-			event.getScreen().setDirty(true);
-			((SpoutPlayer) event.getPlayer()).getMainScreen().attachWidget(null,((GenericLabel) new GenericLabel("I'm on the main screen!").setAlignY(Align.FIRST)).setAlignX(Align.FIRST).setX(0).setY(0).setHeight(427).setWidth(240));
+			if (tries > 3) {}
+				((SpoutPlayer) event.getPlayer()).getMainScreen().closePopup();
+				event.getScreen().setDirty(true);
+				((SpoutPlayer) event.getPlayer()).getMainScreen().attachWidget(null,((GenericLabel) new GenericLabel("I'm on the main screen!").setAlignY(Align.FIRST)).setAlignX(Align.FIRST).setX(0).setY(0).setHeight(427).setWidth(240));
+			}
+			else {
+				tries++;
+				event.getButton().setText("Haha, Try Again!").setDirty(true);
+			}
 			event.getPlayer().sendMessage("Button test successful!");
 		}
 	}
