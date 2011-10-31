33a379e81d97c3ba2179153aa3cb508df1887a38
Afforess <afforess@gmail.com>
2011-10-31 14:27:08 -0300

    remove custom block

--- src/main/java/org/getspout/testplugin/TestBlock.java
+++ src/main/java/org/getspout/testplugin/TestBlock.java
@@ -5,7 +5,7 @@ import org.getspout.spoutapi.block.design.GenericCubeBlockDesign;
 import org.getspout.spoutapi.block.design.Texture;
 import org.getspout.spoutapi.material.block.GenericCubeCustomBlock;
 
-public class TestBlock extends GenericCubeCustomBlock {
+public class TestBlock/* extends GenericCubeCustomBlock*/ {
 	
 	public static String textureUrl = "http://dl.dropbox.com/u/40267690/quartz.png";
 	public static Texture texture = new Texture(textureUrl, 16, 16, 16);
